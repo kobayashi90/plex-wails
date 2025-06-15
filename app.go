d3180f79b2ac3d49b862cbb796eae57d6d3be7df
Kobayashi <51263756+kobayashi90@users.noreply.github.com>
2025-06-15 20:03:43 +0200

    Update app.go

--- app.go
+++ app.go
@@ -1,97 +1,14 @@
 package main
 
-import (
-    "context"
-    "embed"
-    "github.com/wailsapp/wails/v2"
-    "github.com/wailsapp/wails/v2/pkg/options"
-    "github.com/wailsapp/wails/v2/pkg/options/assetserver"
-    "github.com/wailsapp/wails/v2/pkg/runtime"
-)
-
-//go:embed all:frontend/dist
-var assets embed.FS
-
-type App struct {
-    ctx context.Context
-    isFullscreen bool
-}
-
-func NewApp() *App {
-    return &App{
-        isFullscreen: false,
-    }
-}
-
-func (a *App) OnStartup(ctx context.Context) {
-    a.ctx = ctx
-}
-
-func (a *App) SetFullscreen(fullscreen bool) {
-    if fullscreen {
-        runtime.WindowFullscreen(a.ctx)
-        a.isFullscreen = true
-    } else {
-        runtime.WindowUnfullscreen(a.ctx)
-        a.isFullscreen = false
-    }
-}
-
-func (a *App) IsFullscreen() bool {
-    // Use internal state as runtime.WindowIsFullscreen might not be reliable
-    return a.isFullscreen
-}
-
-func (a *App) ToggleFullscreen() {
-    if a.isFullscreen {
-        a.SetFullscreen(false)
-    } else {
-        a.SetFullscreen(true)
-    }
-}
-
-// Minimize window
-func (a *App) MinimizeWindow() {
-    runtime.WindowMinimise(a.ctx)
-}
-
-// Close application
-func (a *App) CloseApplication() {
-    runtime.Quit(a.ctx)
-}
-
-// Restore window from minimized state
-func (a *App) RestoreWindow() {
-    runtime.WindowUnminimise(a.ctx)
-}
-
-// Hide window
-func (a *App) HideWindow() {
-    runtime.WindowHide(a.ctx)
-}
-
-// Show window
-func (a *App) ShowWindow() {
-    runtime.WindowShow(a.ctx)
-}
+import "github.com/webview/webview"
 
 func main() {
-    app := NewApp()
+    debug := true
+    w := webview.New(debug)
+    defer w.Destroy()
 
-    err := wails.Run(&options.App{
-        Title:  "Plex Desktop",
-        Width:  1280,
-        Height: 720,
-        AssetServer: &assetserver.Options{
-            Assets: assets,
-        },
-        OnStartup: app.OnStartup,
-        // Ensure the window can go fullscreen
-        DisableResize: false,
-        Fullscreen:    true,
-    })
-
-    if err != nil {
-        println("Error:", err.Error())
-    }
+    w.SetTitle("Discord")
+    w.SetSize(1280, 800, webview.HintNone)
+    w.Navigate("https://discord.com/app")
+    w.Run()
 }
